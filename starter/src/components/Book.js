2ea48a643d8d9b0d4deeb9117876f517d46b065d
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-12-01 19:28:18 +0000

    delete coments

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -14,18 +14,6 @@ export const Book = ({ book, onChangeShelf }) => {
                 >
                 </div>
                 <ChangeSection book={book} onChangeShelf={onChangeShelf}/>
-                {/* <div className="book-shelf-changer">
-                    <select 
-                    defaultValue={book.shelf} 
-                    onChange={(event) => onChangeShelf(book, event.target.value)}
-                    >
-                        <option value="none" disabled> Move to... </option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                    </select>
-                </div> */}
             </div>
             <div className="book-title">{book.title}</div>
             <div className="book-authors">{book.authors}</div>
