b79a1b11181629b151e3bc6634584064565e2fab
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-12-02 09:57:08 +0000

    Add search functionality

--- starter/src/App.js
+++ starter/src/App.js
@@ -3,18 +3,46 @@ import { useEffect, useState } from "react";
 import * as BooksAPI from "./BooksAPI";
 import { Title } from "./components/Title";
 import { AllShelves } from "./components/AllShelves";
+import { Book } from "./components/Book";
 
 function App() {
 
   const [showSearchPage, setShowSearchpage] = useState(false);
-
   const [books, setBooks] = useState([])
+  const [searchBooks, setSearchBooks] = useState([])
+  const [query, setQuery] = useState("")
+
+  // removes whitespace from both sides of a string and does not change the original string
+  const updateQuery = (query) => {
+    setQuery(query.trim())
+  }
+
   // getting all the books from API
   useEffect(() => {
     BooksAPI.getAll()
     .then(data => setBooks(data))
   }, [])
+  
+  // getting books when search for them
+  // everytime the query changes, the function will run
+  useEffect(() => {
+    // avoid undefined value for the query
+    if (query) {
+      BooksAPI.search(query, 20)
+      .then(data => {
+        if (data.error) {
+          setSearchBooks([])
+        } else {
+          setSearchBooks(data)
+        }
+      })
+    }
+    return () => {
+      setSearchBooks([])
+    }
+  }, [query])
 
+  // update the books in the backend to make sure they will still there when refresh the page
   const onChangeShelf = (book, newShelf) => {
     const updatedBooks = books.map(eachBook => {
       if(eachBook.id === book.id){
@@ -24,7 +52,6 @@ function App() {
       return eachBook
     })
     setBooks(updatedBooks)
-    // update the books in the backend to make sure they will still there when refresh the page
     BooksAPI.update(book, newShelf)
   }
    
@@ -43,11 +70,20 @@ function App() {
               <input
                 type="text"
                 placeholder="Search by title, author, or ISBN"
+                value={query}
+                onChange={(event) => updateQuery(event.target.value)}
               />
             </div>
           </div>
           <div className="search-books-results">
-            <ol className="books-grid"></ol>
+            <ol className="books-grid">
+            {searchBooks.map(eachBook => (
+              // add unique key for each book
+              <li key={eachBook.id}>
+                <Book book={eachBook} onChangeShelf={onChangeShelf}/>
+              </li>
+            ))}
+            </ol>
           </div>
         </div>
       ) : (
